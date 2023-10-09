3bd1db8c03322151646d3f416f4ba8f2b3edf736
DavidDemory <david.demory0@gmail.com>
2023-10-09 16:27:54 +0200

    7th stage 1st attempt

--- app/main.js
+++ app/main.js
@@ -1,4 +1,6 @@
 const net = require("net");
+const fs = require("fs");
+const path = require("path");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -38,6 +40,16 @@ const server = net.createServer((socket) => {
     } else if (index === '/user-agent') {
       const userAgentHeader = retrieveUserAgentHeader(data);
       response = `HTTP/1.1 200 OK\r\nContent-type: text/plain\r\nContent-length: ${userAgentHeader.length}\r\n\r\n${userAgentHeader}`;
+    } else if (index.startsWith('/files')) {
+      const directoryName = process.argv[3];
+      const directory = path.resolve(directoryName);
+      if(fs.existsSync(directory)) {
+        const fileName = index.replace('/files/', '');
+        const filePath = path.resolve(directory, fileName);
+        if(fs.existsSync(filePath)) {
+          response = `HTTP/1.1 200 OK\r\nContent-type: application/octet-stream\r\n\r\n${fs.readFileSync(filePath)}`;
+        }
+      }
     }
 
     socket.write(response, 'utf-8', () => {
