6be72f331ce786ddf714fe55cac4f054c3eede67
DavidDemory <david.demory0@gmail.com>
2023-10-09 14:52:06 +0200

    Socket working

--- app/main.js
+++ app/main.js
@@ -5,11 +5,12 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    socket.write("HTTP/1.1 200 OK\\r\\n\\r\\n", 'utf-8', () => {
+    socket.write('HTTP/1.1 200 OK\r\n\r\n', 'utf-8', () => {
       socket.end();
       server.close();
     });
   });
+
   socket.on("close", () => {
     socket.end();
     server.close();
