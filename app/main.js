b91c802257425c3f1a32cdccbfb73f8733700132
DavidDemory <david.demory0@gmail.com>
2023-10-09 15:18:30 +0200

    Pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -3,9 +3,25 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const parseIndex = (data) => {
+  const parsedLines = data.toString().split("\r\n");
+  const parsedRequest = parsedLines[0].split(" ");
+  const index = parsedRequest[1];
+
+  return index;
+}
+
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    socket.write('HTTP/1.1 200 OK\r\n\r\n', 'utf-8', () => {
+    const index = parseIndex(data);
+
+    let response = 'HTTP/1.1 404 Not Found\r\n\r\n';
+
+    if(index === '/'){
+     response = 'HTTP/1.1 200 OK\r\n\r\n';
+    }
+
+    socket.write(response, 'utf-8', () => {
       socket.end();
       server.close();
     });
